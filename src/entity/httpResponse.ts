680e68ab6e9ce37389488b7f2478552584c738f9
kimsijun <papaya9349@naver.com>
2022-10-16 22:51:00 +0900

    naver news api

---
+++ src/entity/httpResponse.ts
@@ -0,0 +1,11 @@
+export class HttpResponse {
+  // constructor(status, data, message) {
+  //   this.status = status;
+  //   this.data = data;
+  //   this.message = message;
+  // }
+
+  status?: number;
+  data?: string;
+  message?: string;
+}
--- src/naver/naver.service.ts
+++ src/naver/naver.service.ts
@@ -1,7 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { ConfigType } from '@nestjs/config';
 import naverConfig from 'src/config/naverConfig';
+import { HttpResponse } from 'src/entity/httpResponse';
+import convert from 'xml-js';
+import { XMLParser, XMLBuilder, XMLValidator } from 'fast-xml-parser';
 
 @Injectable()
 export class NaverService {
@@ -9,18 +12,33 @@ export class NaverService {
     @Inject(naverConfig.KEY) private config: ConfigType<typeof naverConfig>,
   ) {}
 
-  async getNaverNews(keyword: string) {
-    const client_id = this.config.client_id;
-    const client_secret = this.config.client_secret;
-    const result = axios.get(
-      `https://openapi.naver.com/v1/search/news.xml?query=${keyword}&display=1&start=1&sort=date`,
-      {
-        headers: {
-          'X-Naver-Client-Id': client_id,
-          'X-Naver-Client-Secret': client_secret,
-        },
+  getNaverApiConfiguration(keyword: string): AxiosRequestConfig {
+    const querystring = `${encodeURI(keyword)}&display=100&start=1&sort=date`;
+    const uri = `https://openapi.naver.com/v1/search/news.xml?query=${querystring}`;
+    return {
+      url: uri,
+      headers: {
+        'X-Naver-Client-Id': this.config.client_id,
+        'X-Naver-Client-Secret': this.config.client_secret,
       },
-    );
+    };
+  }
+
+  async getNaverNews(keyword: string): Promise<HttpResponse> {
+    const result: HttpResponse = new HttpResponse();
+    const configuration = this.getNaverApiConfiguration(keyword);
+
+    try {
+      const response = await axios.get(configuration.url, {
+        headers: configuration.headers,
+      });
+      result.status = response.status;
+      console.log(response.data);
+      result.data = new XMLParser().parse(response.data);
+      result.message = 'success';
+    } catch (error) {
+      console.error(error);
+    }
 
     return result;
   }
