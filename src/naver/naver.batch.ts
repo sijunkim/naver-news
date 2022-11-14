cb93c019cee5928e67290b4a00663593e49e06d9
kimsijun <papaya9349@naver.com>
2022-11-14 22:05:09 +0900

    뉴스 화면 출력 로직 제거

--- src/naver/naver.batch.ts
+++ src/naver/naver.batch.ts
@@ -15,7 +15,6 @@ export default class NaverBatch {
   async breakingNewsCron() {
     const result = await this.breakingNewsService.getNaverNews('속보');
     const breakingNews: Array<News> = await this.breakingNewsService.getBreakingNews(result.data);
-    console.log(breakingNews);
     if (breakingNews.length > 0) await this.breakingNewsService.sendNaverNewsToSlack(breakingNews);
   }
 
@@ -23,7 +22,6 @@ export default class NaverBatch {
   async exclusiveNewsCron() {
     const result = await this.exclusiveNewsService.getNaverNews('단독');
     const exclusiveNews: Array<News> = await this.exclusiveNewsService.getExclusiveNews(result.data);
-    console.log(exclusiveNews);
     if (exclusiveNews.length > 0) await this.exclusiveNewsService.sendNaverNewsToSlack(exclusiveNews);
   }
 
