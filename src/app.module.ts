c986e58be9bbd7f71b0254b7bb36aedbb97cae54
kimsijun <papaya9349@naver.com>
2023-01-17 22:24:08 +0900

    app module 내용 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import naverConfig from './config/naverConfig';
-import slackConfig from './config/slackConfig';
+import * as configModule from './config/configModule';
 import { validationSchema } from './config/validationSchema';
 import { NaverController } from './naver/news.controller';
 import { BreakingNewsService } from './naver/breaking-news.service';
 import { ExclusiveNewsService } from './naver/exclusive-news.service';
 import SlackWebhook from './common/util/slackWebhook';
 import NewsRefiner from './common/util/newsRefiner';
-import NaverBatch from './naver/news.batch';
+import NewsBatch from './naver/news.batch';
 import { ScheduleModule } from '@nestjs/schedule';
+import { NewsService } from './naver/news.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [naverConfig, slackConfig],
+      load: [configModule.fileConfig, configModule.naverConfig, configModule.slackConfig],
       isGlobal: true,
       validationSchema,
     }),
     ScheduleModule.forRoot(),
   ],
   controllers: [AppController, NaverController],
-  providers: [AppService, BreakingNewsService, ExclusiveNewsService, NaverBatch, SlackWebhook, NewsRefiner],
+  providers: [AppService, NewsService, BreakingNewsService, ExclusiveNewsService, NewsBatch, SlackWebhook, NewsRefiner],
 })
 export class AppModule {}
