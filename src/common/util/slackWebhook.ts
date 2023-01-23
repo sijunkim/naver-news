6116ec58d2878908a6b70ec28833c983108ab76e
kimsijun <papaya9349@naver.com>
2023-01-23 21:29:33 +0900

    webhook url 초기화 로직 변경

--- src/common/util/slackWebhook.ts
+++ src/common/util/slackWebhook.ts
@@ -13,16 +13,21 @@ export default class SlackWebhook {
   ) {}
 
   async newsSend(newsType: NEWSTYPE, payloads: IncomingWebhookSendArguments) {
+    let webhook = new IncomingWebhook(undefined);
+
+    // NEWSBOT
     const breakingNewsWebhookUrl: string = this.slackConfig.breakingNewsWebhookUrl;
     const exclusiveNewsWebhookUrl: string = this.slackConfig.exclusiveNewsWebhookUrl;
     const url: string = newsType == BreakingNewsType ? breakingNewsWebhookUrl : exclusiveNewsWebhookUrl;
-
-    // NEWSBOT
-    let webhook = new IncomingWebhook(url);
+    webhook = new IncomingWebhook(url);
     await webhook.send(payloads);
 
     // CHAINPARTNERS
     webhook = new IncomingWebhook(this.slackConfig.chainpartnersNewsWebhookUrl);
     await webhook.send(payloads);
+
+    // // TEST
+    // webhook = new IncomingWebhook(this.slackConfig.developWebhookUrl);
+    // await webhook.send(payloads);
   }
 }
